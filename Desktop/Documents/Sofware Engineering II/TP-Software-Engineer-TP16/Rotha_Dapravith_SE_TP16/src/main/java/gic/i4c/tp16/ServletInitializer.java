ecaa45171076d57a530a5a5ed44fb6e2e208300e
Rotha Dapravith <rothadapravith@MacbookPro-M1.local>
2023-04-04 21:54:48 +0700

    TP16-V2

---
+++ Desktop/Documents/Sofware Engineering II/TP-Software-Engineer-TP16/Rotha_Dapravith_SE_TP16/src/main/java/gic/i4c/tp16/ServletInitializer.java
@@ -0,0 +1,14 @@
+package gic.i4c.tp16;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Tp16Application.class);
+	}
+
+	
+}
